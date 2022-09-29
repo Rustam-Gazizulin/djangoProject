ce19807ee6b15a3e363ebf4662c6f730bb1cd2e9
rgazizulin174 <rgazizulin174@gmail.com>
2022-09-29 20:18:11 +0500

    add postgres test1

--- vacancies/views.py
+++ vacancies/views.py
@@ -1,20 +1,19 @@
 import json
 
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, DeleteView, UpdateView
 
-from vacancies.models import Vacancy
+from vacancies.models import Vacancy, Skill
 
 
 def hello(request):
     return HttpResponse("Hello World")
 
 
-class VacancyView(ListView):
+class VacancyListView(ListView):
     model = Vacancy
 
     def get(self, request, *args, **kwargs):
@@ -42,7 +41,11 @@ class VacancyDetailView(DetailView):
 
         return JsonResponse({
             "id": vacancy.id,
-            "text": vacancy.text
+            "text": vacancy.text,
+            "slug": vacancy.slug,
+            "status": vacancy.status,
+            "created": vacancy.created,
+            "user": vacancy.user_id
         })
 
 
@@ -67,13 +70,47 @@ class VacancyCreateView(CreateView):
         })
 
 
-class VacancyDetailView(DetailView):
+@method_decorator(csrf_exempt, name="dispatch")
+class VacancyUpdateView(UpdateView):
     model = Vacancy
+    fields = ["slug", "text", "status", "skills"]
 
-    def get(self, request, *args, **kwargs):
-        vacancy = self.get_object()
+    def patch(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+
+        vacancy_data = json.loads(request.body)
+
+        self.object.slug = vacancy_data["slug"]
+        self.object.text = vacancy_data["text"]
+        self.object.status = vacancy_data["status"]
+
+        for skill in vacancy_data["skills"]:
+            try:
+                skill_obj = Skill.objects.get(name=skill)
+            except Skill.DoesNotExist:
+                return JsonResponse({"error": "Skill not found"}, status=404)
+            self.object.skills.add(skill_obj)
+
+        self.object.save()
 
         return JsonResponse({
-            "id": vacancy.id,
-            "text": vacancy.text
+            "id": self.object.id,
+            "text": self.object.text,
+            "slug": self.object.slug,
+            "status": self.object.status,
+            "created": self.object.created,
+            "user": self.object.user_id,
+            "skills": list(self.object.skills.all().values_list("name", flat=True)),
+
         })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class VacancyDeleteView(DeleteView):
+    model = Vacancy
+    success_url = "/"
+
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+
+        return JsonResponse({"status": "Ok"}, status=200)
