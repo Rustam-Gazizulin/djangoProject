0910eaa0a3193918d25d5bc15f377be2caeca8fd
rgazizulin174 <rgazizulin174@gmail.com>
2022-10-05 20:52:16 +0500

    add image, logo fix

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,16 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from djangoProject import settings
 from vacancies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
     path('vacancy/', include('vacancies.urls')),
+    path('company/', include('companies.urls')),
 ]
 
 if settings.DEBUG:
