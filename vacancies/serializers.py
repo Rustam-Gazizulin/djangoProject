a7fc58fb88270879eff9786e30d86d7ee26400d7
rgazizulin174 <rgazizulin174@gmail.com>
2022-10-14 19:05:36 +0500

    add serializator1

--- vacancies/serializers.py
+++ vacancies/serializers.py
@@ -1,10 +1,38 @@
 from rest_framework import serializers
 
+from vacancies.models import Vacancy
 
-class VacancySerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    text = serializers.CharField(max_length=2000)
-    slug = serializers.CharField(max_length=50)
-    status = serializers.CharField(max_length=6)
-    created = serializers.DateField()
-    username = serializers.CharField(max_length=100)
+
+class VacancyListSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    skills = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+
+    class Meta:
+        model = Vacancy
+        fields = ['id', 'text', 'slug', 'status', 'created', 'username', 'skills']
+
+
+class VacancyDetailSerializer(serializers.ModelSerializer):
+    
+    skills = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name")
+
+    class Meta:
+        model = Vacancy
+        fields = '__all__'
+
+
+class VacancyCreateSerializer(serializers.ModelSerializer):
+
+    id = serializers.IntegerField(required=False)
+
+    class Meta:
+        model = Vacancy
+        exclude = ['skills']
+
--- vacancies/views.py
+++ vacancies/views.py
@@ -12,7 +12,7 @@ from django.views.generic import DetailView, ListView, CreateView, DeleteView, U
 
 from djangoProject import settings
 from vacancies.models import Vacancy, Skill
-from vacancies.serializers import VacancySerializer
+from vacancies.serializers import VacancyDetailSerializer, VacancyListSerializer, VacancyCreateSerializer
 
 
 def hello(request):
@@ -35,22 +35,10 @@ class VacancyListView(ListView):
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
 
-        # vacancies = []
-        # for vacancy in page_obj:
-        #     vacancies.append({
-        #         "id": vacancy.id,
-        #         "text": vacancy.text,
-        #         "slug": vacancy.slug,
-        #         "status": vacancy.status,
-        #         "created": vacancy.created,
-        #         "user": vacancy.user.username,
-        #         "skills": list(map(str, vacancy.skills.all())),
-        #
-        #     })
         list(map(lambda x: setattr(x, "username", x.user.username if x.user else None), page_obj))
 
         response = {
-            "items": VacancySerializer(page_obj, many=True).data,
+            "items": VacancyListSerializer(page_obj, many=True).data,
             "num_pages": paginator.num_pages,
             "total": paginator.count
         }
@@ -64,15 +52,7 @@ class VacancyDetailView(DetailView):
     def get(self, request, *args, **kwargs):
         vacancy = self.get_object()
 
-        return JsonResponse({
-            "id": vacancy.id,
-            "text": vacancy.text,
-            "slug": vacancy.slug,
-            "status": vacancy.status,
-            "created": vacancy.created,
-            "user": vacancy.user_id,
-            "skills": list(vacancy.skills.all().values_list("name", flat=True)),
-        })
+        return JsonResponse(VacancyDetailSerializer(vacancy).data)
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -81,31 +61,13 @@ class VacancyCreateView(CreateView):
     fields = ["user", "slug", "text", "status", "created", "skills"]
 
     def post(self, request, *args, **kwargs):
-        vacancy_data = json.loads(request.body)
+        vacancy_data = VacancyCreateSerializer(data=json.loads(request.body))
+        if vacancy_data.is_valid():
+            vacancy_data.save()
+        else:
+            return JsonResponse(vacancy_data.errors)
 
-        vacancy = Vacancy.objects.create(
-
-            slug=vacancy_data["slug"],
-            text=vacancy_data["text"],
-            status=vacancy_data["status"]
-
-        )
-        vacancy.user = get_object_or_404(User, pk=vacancy_data["user_id"])
-
-        for skill in vacancy_data["skills"]:
-            skill_obj, created = Skill.objects.get_or_create(name=skill, defaults={"is_active": True})
-            vacancy.skills.add(skill_obj)
-        vacancy.save()
-
-        return JsonResponse({
-            "id": vacancy.id,
-            "text": vacancy.text,
-            "slug": vacancy.slug,
-            "status": vacancy.status,
-            "created": vacancy.created,
-            "user": vacancy.user_id,
-
-        })
+        return JsonResponse(vacancy_data.data)
 
 
 @method_decorator(csrf_exempt, name="dispatch")
