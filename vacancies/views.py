c9b9db246ecbe6cae711d1e87879e96bbf6a6fce
rgazizulin174 <rgazizulin174@gmail.com>
2022-10-19 11:14:46 +0500

    add find many skills

--- vacancies/views.py
+++ vacancies/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-from django.db.models import Count, Avg
+from django.db.models import Count, Avg, Q
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
@@ -32,11 +32,15 @@ class VacancyListView(ListAPIView):
                 text__icontains=vacancy_text
             )
 
-        skill_name = request.GET.get('skill', None)
-        if skill_name:
-            self.queryset = self.queryset.filter(
-                skills__name__icontains=skill_name
-            )
+        skills = request.GET.getlist('skill', None)
+        skills_q = None
+        for skill in skills:
+            if skills_q is None:
+                skills_q = Q(skills__name__icontains=skill)
+            else:
+                skills_q |= Q(skills__name__icontains=skill)
+        if skills_q:
+            self.queryset = self.queryset.filter(skills_q)
 
         return super().get(request, *args, **kwargs)
 
