8f45f70bf43064d7ac954ee7a0082c31d61c9f72
rgazizulin174 <rgazizulin174@gmail.com>
2022-10-19 10:27:28 +0500

    add router

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,8 +17,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
 from vacancies import views
+from vacancies.views import SkillsViewSet
+
+router = routers.SimpleRouter()
+router.register('skill', SkillsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +32,8 @@ urlpatterns = [
     path('vacancy/', include('vacancies.urls')),
     path('company/', include('companies.urls')),
 ]
+
+urlpatterns += router.urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- vacancies/views.py
+++ vacancies/views.py
@@ -4,15 +4,21 @@ from django.db.models import Count, Avg
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from djangoProject import settings
-from vacancies.models import Vacancy
+from vacancies.models import Vacancy, Skill
 from vacancies.serializers import VacancyDetailSerializer, VacancyListSerializer, VacancyCreateSerializer, \
-    VacancyUpdateSerializer, VacancyDestroySerializer
+    VacancyUpdateSerializer, VacancyDestroySerializer, SkillSerializer
 
 
 def hello(request):
     return HttpResponse("Hello World")
+
+
+class SkillsViewSet(ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
 
 
 class VacancyListView(ListAPIView):
