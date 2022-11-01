cd61bec07a1bf95b7b7315c4f74f1d5b4d957fb8
rgazizulin174 <rgazizulin174@gmail.com>
2022-11-01 17:32:02 +0500

    add pytest 3

---
+++ tests/vacancies/vacancy_create_test.py
@@ -0,0 +1,36 @@
+from datetime import date
+
+import pytest
+
+
+@pytest.mark.django_db
+def test_create_vacancy(client, hr_token):
+    expected_response = {
+        'id': 2,
+        'skills': [],
+        'created': date.today().strftime('%Y-%m-%d'),
+        'text': 'test1',
+        'slug': 'test1',
+        'status': 'draft',
+        'min_experience': None,
+        'likes': 0,
+        'updated_at': None,
+        'user': None
+    }
+
+    data = {
+        'text': 'test1',
+        'slug': 'test1',
+        'status': 'draft'
+    }
+
+    response = client.post(
+        '/vacancy/create/',
+        data,
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token ' + hr_token
+
+    )
+
+    assert response.status_code == 201
+    assert response.data == expected_response
